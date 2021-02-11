8cf66ed77c30cdba81a44554e9c0a8c9cab7b9ae
gonzalo10 <gonzalo.achiaga@gmail.com>
2021-02-11 13:37:08 +0100

    feat: reverted to Alfonso version of the tank

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-// import Game from "./ammo-tank";
+import Game from './ammo-tank'
 // import Game from "./car";
-import Game from "./car-clean";
+// import Game from "./car-clean";
 // import Game from "./tank";
 
-export default Game;
+export default Game
